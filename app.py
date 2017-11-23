dd8e3fc74b968c8d197f0ec397c0b5f8efcaba6a
py <advith.nagappa@gmail.com>
2017-11-23 06:45:10 +0100

    rm tests

--- app.py
+++ app.py
@@ -1,40 +1,35 @@
 import os
 from datetime import datetime
-from flask import Flask
+
+from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
-#import config
-#######################################
-
-
-#######################################
 
 from models import User
 import config
+
+from extensions import db
+from api import registerApi,signApi,serviceApi
+
+
 def create_app(config_name):
     config_object = ".".join(('config',config_name))
     app = Flask(__name__)
     app.config.from_object(config_object)
-    #import apis.views
-    from extensions import db
+
+    
     db.init_app(app)
-    from api import registerApi,signApi,serviceApi
+
+    
     app.register_blueprint(registerApi)    
     app.register_blueprint(signApi)
     app.register_blueprint(serviceApi)
+
     return app
 
 
-#######################################
-
-'''def create_app(config_name):
-    config_object = ".".join(('config',config_name))
-    app = Flask(__name__)     
-    app.config.from_object(config_object)
-    db.init_app(app)
-    return app'''
-
-#######################################
-
+def init_db():
+    with current_app.context():
+        db.create_all()
 
 
 
